6c5ab49e82cfc03da8672d34fc89880beb065ae7
John Ramey <johnramey@gmail.com>
2010-10-11 21:38:41 -0500

    Initial commit of FG-algorithm from Flury and Gautschi (1986).

---
+++ fg.r
@@ -0,0 +1,7 @@
+# FG-algorithm
+# Given in Flury and Gautschi (1986).
+# Simultaneously transforms k positive definite matrices of dimension p x p to near diagonal form.
+
+fg <- function(...) {
+	
+}
